a4dfb1f5b0cf8cea5025dcf2bc1030a56bbc7651
Hector Toral <hectortoraltrabajos@gmail.com>
2024-09-18 16:02:25 +0200

    disable eslint during build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 }
 
